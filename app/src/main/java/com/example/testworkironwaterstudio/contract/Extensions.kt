cd0e9a71fe33337dcf8d9b95d7f0591a3c03851c
rusrst <69162196+rusrst@users.noreply.github.com>
2022-04-06 23:38:15 +0300

    add open browser

--- app/src/main/java/com/example/testworkironwaterstudio/contract/Extensions.kt
+++ app/src/main/java/com/example/testworkironwaterstudio/contract/Extensions.kt
@@ -1,8 +1,26 @@
 package com.example.testworkironwaterstudio.contract
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 
 const val ARG_STARTUP = "ARG_STARTUP"
 const val INPUT_DATA = "BUNDLE"
 
 fun Fragment.updateUi() = (requireActivity() as ActionUI).updateUi()
+
+fun String?.asUri(): Uri? {
+    return try {
+        Uri.parse(this)
+    } catch (e: Exception) {
+        null
+    }
+}
+
+fun Uri?.openInBrowser(context: Context) {
+    this ?: return // Do nothing if uri is null
+    val browserIntent = Intent(Intent.ACTION_VIEW, this)
+    ContextCompat.startActivity(context, browserIntent, null)
+}
--- app/src/main/java/com/example/testworkironwaterstudio/fragments/AboutFragment.kt
+++ app/src/main/java/com/example/testworkironwaterstudio/fragments/AboutFragment.kt
@@ -1,12 +1,16 @@
 package com.example.testworkironwaterstudio.fragments
 
+import android.content.DialogInterface
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentResultListener
 import com.example.testworkironwaterstudio.R
 import com.example.testworkironwaterstudio.contract.HasCustomTitle
+import com.example.testworkironwaterstudio.contract.asUri
+import com.example.testworkironwaterstudio.contract.openInBrowser
 import com.example.testworkironwaterstudio.databinding.AboutFragmentBinding
 
 class AboutFragment: Fragment(), HasCustomTitle {
@@ -23,6 +27,7 @@ class AboutFragment: Fragment(), HasCustomTitle {
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupSimpleDialogFragmentListener()
         binding.aboutButton.setOnClickListener {
             showDialogBrowser()
         }
@@ -34,6 +39,17 @@ class AboutFragment: Fragment(), HasCustomTitle {
         args.putString("text", getString(R.string.open_browser))
         simpleDialog.arguments = args
         simpleDialog.show(requireActivity().supportFragmentManager, SimpleDialogFragment.TAG)
+    }
+
+    private fun setupSimpleDialogFragmentListener() {
+        requireActivity().supportFragmentManager.setFragmentResultListener(SimpleDialogFragment.KEY,
+            viewLifecycleOwner) { _, result ->
+            when (result.getInt(SimpleDialogFragment.TAG)) {
+                DialogInterface.BUTTON_POSITIVE -> {
+                    getString(R.string.uri).asUri().openInBrowser(requireContext())
+                }
+            }
+        }
     }
 
     override fun getTitle(): String {
